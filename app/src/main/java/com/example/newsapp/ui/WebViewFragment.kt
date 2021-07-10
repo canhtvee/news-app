ecb761ec63b16b0b8e03309a380c847c6de7532d
kant1310 <canhtv.ee@gmail.com>
2021-07-10 20:18:55 +0300

    progress bar ok, modified on dell 10072021_rev2

--- app/src/main/java/com/example/newsapp/ui/WebViewFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/WebViewFragment.kt
@@ -1,6 +1,7 @@
 package com.example.newsapp.ui
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,12 +11,14 @@ import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import com.example.newsapp.R
 import com.example.newsapp.viewmodels.WebViewModel
+import com.google.android.material.progressindicator.LinearProgressIndicator
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -36,14 +39,35 @@ class WebViewFragment : Fragment() {
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
+        progressBar.progress = 0
         val webView = view.findViewById<WebView>(R.id.webView).apply {
             settings.javaScriptEnabled = true
-            webViewClient = WebViewClient()
-            webChromeClient = WebChromeClient()
         }
 
-        webViewModel.viewData.observe(viewLifecycleOwner, Observer {
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+                progressBar.progress = newProgress
+            }
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressBar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.visibility = View.INVISIBLE
+            }
+        }
+
+        webViewModel.viewData.observe(viewLifecycleOwner, {
             webView.loadUrl(it.url)
         })
+
     }
 }
